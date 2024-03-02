b3b195eedd6c1aa916a74c59a8087bb1e4e19e05
august-se <lithium0813@live.com>
2024-03-02 18:40:56 +0100

    reset omnipaxos_durability.rs

--- src/durability/omnipaxos_durability.rs
+++ src/durability/omnipaxos_durability.rs
@@ -1,55 +1,9 @@
-use crate::datastore::{tx_data::TxData, TxOffset};
 use super::*;
-
-use std::{
-    fs::File,
-    io::{Read, Write},
-    sync::{Arc, Mutex},
-};
-use tokio::{
-    fs::{File as AsyncFile, OpenOptions as AsyncOpenOptions},
-    io::AsyncWriteExt,
-    sync::mpsc,
-};
-struct LogEntry {
-    tx_offset: TxOffset,
-    tx_data: TxData,
-}
-
-impl LogEntry {
-    fn serialize<W: Write>(&self, writer: &mut W) -> std::io::Result<()> {
-        writer.write_all(&self.tx_offset.0.to_le_bytes())?;
-        self.tx_data.serialize(writer)
-    }
-
-    fn deserialize<R: Read>(reader: &mut R) -> std::io::Result<Self> {
-        let mut bytes = [0; 8];
-        reader.read_exact(&mut bytes)?;
-        let tx_offset = TxOffset(u64::from_le_bytes(bytes));
-        let tx_data = TxData::deserialize(reader)?;
-        Ok(LogEntry { tx_offset, tx_data })
-    }
-}
 
 /// OmniPaxosDurability is a OmniPaxos node that should provide the replicated
 /// implementation of the DurabilityLayer trait required by the Datastore.
 pub struct OmniPaxosDurability {
-    // TODO    
-    append_tx: Option<mpsc::Sender<LogEntry>>,
-    durable_offset: Arc<Mutex<Option<TxOffset>>>,
-    log_file: Arc<Mutex<Option<AsyncFile>>>,
-    filepath: String,
-}
-
-impl OmniPaxosDurability {
-    pub fn new(filepath: &str) -> Self {
-        OmniPaxosDurability {
-            append_tx: None,
-            durable_offset: Arc::new(Mutex::new(None)),
-            log_file: Arc::new(Mutex::new(None)),
-            filepath: filepath.to_string(),
-        }
-    }
+    // TODO
 }
 
 impl DurabilityLayer for OmniPaxosDurability {
