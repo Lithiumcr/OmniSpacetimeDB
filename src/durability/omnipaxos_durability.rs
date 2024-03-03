f4378926768b99405d6bd655b732d02e0cc84563
Minh Duc Pham <m.ducpham1902@gmail.com>
2024-03-03 22:31:41 +0000

    resolve conflicts

--- src/durability/omnipaxos_durability.rs
+++ src/durability/omnipaxos_durability.rs
@@ -1,3 +1,4 @@
+use crate::datastore::{tx_data::TxData, TxOffset};
 use super::*;
 
 use crate::datastore::{tx_data::TxData, TxOffset};
@@ -7,25 +8,39 @@ use self::example_durability::ExampleDurabilityLayer;
 /// implementation of the DurabilityLayer trait required by the Datastore.
 pub struct OmniPaxosDurability {
     // TODO
+    append_tx: Option<mpsc::Sender<LogEntry>>,
+    durable_offset: Arc<Mutex<Option<TxOffset>>>,
+    log_file: Arc<Mutex<Option<AsyncFile>>>,
+    filepath: String,
+}
+
+impl OmniPaxosDurability {
+    pub fn new(filepath: &str) -> Self {
+        OmniPaxosDurability {
+            append_tx: None,
+            durable_offset: Arc::new(Mutex::new(None)),
+            log_file: Arc::new(Mutex::new(None)),
+            filepath: filepath.to_string(),
+        }
+    }
 }
 
 impl DurabilityLayer for OmniPaxosDurability {
     fn iter(&self) -> Box<dyn Iterator<Item = (TxOffset, TxData)>> {
-        todo!()
+        // Provide implementation here
     }
 
     fn iter_starting_from_offset(
         &self,
         offset: TxOffset,
     ) -> Box<dyn Iterator<Item = (TxOffset, TxData)>> {
-        todo!()
+        // Provide implementation here
     }
 
     fn append_tx(&mut self, tx_offset: TxOffset, tx_data: TxData) {
-        todo!()
+        // Provide implementation here
     }
 
     fn get_durable_tx_offset(&self) -> TxOffset {
-
     }
 }
